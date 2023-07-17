643ba95f9592345654d5c9ad1b5cd34a687ab203
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-17 23:02:48 +0300

    complete test

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev == self
+      freebie.update(dev: dev)
+      puts "Successfully gave away the freebie to #{dev.name}."
+    else
+      puts "You can only give away a freebie that belongs to you."
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+meta = Company.create(name: "Meta", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazor = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,12 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
-
+Freebie.create(item_name: "sticker", value: 10, dev_id: rick.id, company_id: google.id)
+Freebie.create(item_name: "wrist band", value: 7, dev_id: rick.id, company_id: gazor.id)
+Freebie.create(item_name: "hand band", value: 10, dev_id: morty.id, company_id: meta.id)
+Freebie.create(item_name: "necklace", value: 5, dev_id: meseeks.id, company_id: dunder.id)
+Freebie.create(item_name: "leglace", value: 5, dev_id: rick.id, company_id: google.id)
+Freebie.create(item_name: "keyholder", value: 6, dev_id: gazor.id, company_id: google.id)
+Freebie.create(item_name: "keyholder", value: 6, dev_id: rick.id, company_id: meta.id)
+Freebie.create(item_name: "keyholder", value: 6, dev_id: morty.id, company_id: google.id)
 puts "Seeding done!"
